4290e19e9ae16a8e4d97119ad16058c5f3c45d9e
stefi23 <stefania.rosca2309@gmail.com>
2021-01-24 12:48:50 +0200

    added the countdown

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 import './App.css';
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 function App() {
+
+  const [text, setText] = useState("")
+  const [timeRemaining, setTimeRemaining] = useState(3)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+
+      setTimeRemaining(prevState => {
+        if (prevState !== 0) {
+          setTimeRemaining(prevState - 1)
+        } else {
+          setTimeRemaining(0)
+        }
+      })
+
+    }, 1000)
+    return () => clearInterval(interval);
+
+  }, []);
+
+  const handleChange = (e) => {
+    const { value } = e.target
+    setText(value)
+  }
+
+  const calculateWordCount = (text) => {
+    const wordsArr = text.trim().split(" ")
+    const filteredWords = wordsArr.filter(word => word !== "")
+    return filteredWords.length
+  }
+
   return (
-    <>
+    <div>
       <h1>Title</h1>
-      <textarea />
-      <h4>Time remaining: </h4>
-      <button>Start</button>
+      <textarea
+        onChange={handleChange}
+        value={text} />
+      <h4>Time remaining: {timeRemaining} </h4>
+      <button onClick={() => calculateWordCount(text)}>Start</button>
       <h1>Word count:</h1>
-    </>
+    </div>
 
   )
 }
